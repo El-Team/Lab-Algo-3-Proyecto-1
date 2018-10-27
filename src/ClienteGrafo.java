1454912b65013e8b6cc405166616529b227328ba
Luis Paredes <lualps94@gmail.com>
2018-10-26 20:28:18 -0400

    Renamed client class to match specs

---
+++ src/ClienteGrafo.java
@@ -0,0 +1,9 @@
+/**
+ * Programa cliente que permite hacer uso de las funcionalidades de las clases
+ * que implementan la interfaz Grafo.
+ */
+public class ClienteGrafo {
+	public static void main(String[] args) {
+
+	}
+}
