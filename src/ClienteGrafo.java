c1a0dd7039fcf7cecd594472da45f6381568afcc
Luis Paredes <lualps94@gmail.com>
2018-10-29 09:51:35 -0400

    Implemented ClienteGrafo.documentHasValidFormat()

--- src/ClienteGrafo.java
+++ src/ClienteGrafo.java
@@ -3,9 +3,18 @@
  * que implementan la interfaz Grafo.
  */
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.lang.IllegalArgumentException;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.List;
+import java.nio.charset.Charset;
+import java.io.IOException;
+import java.lang.NumberFormatException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.lang.Integer;
 
 public class ClienteGrafo {
 
@@ -24,8 +33,40 @@ public class ClienteGrafo {
 	 * Verifica que el archivo tenga el formato correcto
 	 */
 	private static boolean documentHasValidFormat(String filename) {
-		// use a regex
-		return false;
+		List<String> lines = null;
+		String fileContent = "";
+		Integer n = 0;
+		Integer m = 0;
+
+		try {
+			lines = Files.readAllLines(
+				Paths.get(filename),
+				Charset.defaultCharset()
+			);
+			n = Integer.parseInt(lines.get(3));
+			m = Integer.parseInt(lines.get(4));
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		catch(NumberFormatException e) {
+			System.out.println("Número de vértices o lados inválido");
+		}
+
+		for (String line : lines) {
+			fileContent += line;
+			fileContent += ",";
+			System.out.println(fileContent);
+		}
+
+		String regexStr =
+			"(B|D|S),(B|D|S),(D|N),[0-9]+,[0-9]+,((\\S)+\\s(\\S)+\\s[0-9]+,){" +
+			n.toString() + "}((\\S)+\\s(\\S)+\\s[0-9]+\\s(\\S)+\\s(\\S)+,){" +
+			m.toString() + "}";
+		Pattern regexPattern = Pattern.compile(regexStr);
+		Matcher match = regexPattern.matcher(fileContent);
+
+		return match.matches();
 	}
 
 	private static void displayClientForNoArguments() {
