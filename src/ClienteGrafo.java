24efb924551f442cbd8710c6123f1d4d75172733
Luis Paredes <lualps94@gmail.com>
2018-10-31 12:47:44 -0400

    Implemented basic logic to load graph and initialize prompt in ClienteGrafo

--- src/ClienteGrafo.java
+++ src/ClienteGrafo.java
@@ -13,19 +13,96 @@ import java.io.IOException;
 
 public class ClienteGrafo {
 
+	/**
+	 * Instancia de Grafo que será manejada a través del cliente
+	 */
+	private static Grafo g;
+	private static Boolean sessionIsActive = true;
+
+	/**
+	 * Cliente ejecutado cuando el usuario no pasa argumentos
+	 */
 	private static void displayClientForNoArguments() {
 		System.out.println("No argument");
 	}
 
+	private static void createGraph(String filename) {
+
+		// Abrir archivo de texto
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(
+				Paths.get(filename),
+				Charset.defaultCharset()
+			);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+
+		// Obtener información básica
+		String vertexAndEdgeType = lines.get(0) + lines.get(1);
+		String graphType = lines.get(2);
+		String n = lines.get(3);
+		String m = lines.get(4);
+
+		// Inicializar grafo acorde al tipo
+		if (graphType == "D") {
+			switch (vertexAndEdgeType) {
+				case "BB": g = new GrafoDirigido<Boolean, Boolean>(); break;
+				case "BD": g = new GrafoDirigido<Boolean, Double>(); break;
+				case "BS": g = new GrafoDirigido<Boolean, String>(); break;
+				case "DB": g = new GrafoDirigido<Double, Boolean>(); break;
+				case "DD": g = new GrafoDirigido<Double, Double>(); break;
+				case "DS": g = new GrafoDirigido<Double, String>(); break;
+				case "SB": g = new GrafoDirigido<String, Boolean>(); break;
+				case "SD": g = new GrafoDirigido<String, Double>(); break;
+				case "SS": g = new GrafoDirigido<String, String>(); break;
+			}
+		}
+		else {
+			switch (vertexAndEdgeType) {
+				case "BB": g = new GrafoNoDirigido<Boolean, Boolean>(); break;
+				case "BD": g = new GrafoNoDirigido<Boolean, Double>(); break;
+				case "BS": g = new GrafoNoDirigido<Boolean, String>(); break;
+				case "DB": g = new GrafoNoDirigido<Double, Boolean>(); break;
+				case "DD": g = new GrafoNoDirigido<Double, Double>(); break;
+				case "DS": g = new GrafoNoDirigido<Double, String>(); break;
+				case "SB": g = new GrafoNoDirigido<String, Boolean>(); break;
+				case "SD": g = new GrafoNoDirigido<String, Double>(); break;
+				case "SS": g = new GrafoNoDirigido<String, String>(); break;
+			}
+		}
+
+		g.cargarGrafo(g, filename);
+	}
+
+	/**
+	 * Cónsola que muestra las opciones disponibles y permite al usuario
+	 * interactuar con el grafo cargado
+	 */
+	private static void displayPrompt() {
+		while(sessionIsActive) {
+			System.out.println("prompt works");
+			sessionIsActive = false;
+		}
+	}
+
+	/**
+	 * Cliente ejecutado cuando el usuario pasa argumentos
+	 */
 	private static void displayClientForArguments(String[] args) {
+
+		String filename = args[0];
+
 		try {
 			if (args.length > 1) {
 				throw new IllegalArgumentException();
 			}
-			else if (!Utilidades.isValidPath(args[0])) {
+			else if (!Utilidades.isValidPath(filename)) {
 				throw new FileNotFoundException();
 			}
-			else if (!Utilidades.documentHasValidFormat(args[0])) {
+			else if (!Utilidades.documentHasValidFormat(filename)) {
 				throw new ParseException("", 0);
 			}
 		}
@@ -41,8 +118,15 @@ public class ClienteGrafo {
 			System.out.println("El documento no tiene el formato correcto");
 			System.exit(0);
 		}
+
+		createGraph(filename);
+		displayPrompt();
 	}
 
+	/**
+	 * Rutina principal, se encarga de mostrar el cliente correcto dependiendo
+	 * del número de argumentos detectados
+	 */
 	public static void main(String[] args) {
 
 		if (args.length == 0) {
