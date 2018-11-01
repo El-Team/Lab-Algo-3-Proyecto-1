a2afe13cce49e5c5aa69b7a181afb19ff3ff1ac5
Luis Paredes <lualps94@gmail.com>
2018-11-01 16:17:59 -0400

    Added boilerplate code for remaining methods in GrafoNoDirigido

--- src/GrafoNoDirigido.java
+++ src/GrafoNoDirigido.java
@@ -3,8 +3,9 @@
  * representa a un grafo no dirigido.
  */
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
-public class GrafoNoDirigido<V, L>implements Grafo<V, L> {
+public class GrafoNoDirigido<V, L> implements Grafo<V, L> {
 
 	/**
 	 * Crea un nuevo GrafNoDirigido
@@ -141,4 +142,82 @@ public class GrafoNoDirigido<V, L>implements Grafo<V, L> {
 		return "";
 	}
 
+	/**
+	 * Agrega una nueva arista al grafo si el identificador de la arista no lo posee ninguna arista en el grafo.
+	 * Retorna true en caso en que la inserción se lleve a cabo, false en contrario.
+	 */
+	public boolean agregarArista(Grafo<V,L> g, Arco<L> a) {
+		GrafoDirigido<V,L> castedGraph = (GrafoDirigido<V,L>)g;
+		for (Lado<L> l : castedGraph.lados(this)) {
+			Arco<L> castedEdge = (Arco<L>)l;
+			if (castedEdge.getId() == a.getId()) {
+				return false;
+			}
+		}
+		try {
+			castedGraph.getEdges().put(a.getId(), a);
+		}
+		catch(Error e) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Si el identificador id no lo posee ninguna arista en el grafo, crea una nueva arista y la agrega en el
+	 * grafo. Retorna true en caso en que la inserción se lleve a cabo, false en contrario.
+	 */
+	public boolean agregarArista(
+			Grafo<V,L> g,
+			String id,
+			L dato,
+			double p,
+			String u,
+			String v
+		) {
+
+		try {
+			GrafoDirigido<V,L> castedGraph = (GrafoDirigido<V,L>)g;
+			if (
+				!g.estaVertice(this, u) ||
+				!g.estaVertice(this, v)
+			) {
+				throw new NoSuchElementException();
+			}
+			Vertice<V> vi = g.obtenerVertice(this, u);
+			Vertice<V> vf = g.obtenerVertice(this, v);
+			Arco<L> a = new Arco(id, dato, p, vi, vf);
+			castedGraph.getEdges().put(a.getId(), a);
+		}
+		catch(Error e) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Elimina la arista en el grafo que esté identificada con id. Se retorna true en caso que se haya
+	 * eliminado la arista del grafo y false en caso de que no exista una arista con ese identificador en el
+	 * grafo.
+	 */
+	public boolean eliminarArista(Grafo<V,L> g, String id) {
+		GrafoDirigido<V,L> castedGraph = (GrafoDirigido<V,L>)g;
+		if (castedGraph.getEdges().remove(id) != null) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Devuelve la arista que tiene como identificador id. En caso de que no exista ninguna arista con ese
+	 * identificador, se lanza la excepción NoSuchElementException.
+	 */
+	public Arco obtenerArista(Grafo<V,L> g, String id) throws NoSuchElementException {
+		GrafoDirigido<V,L> castedGraph = (GrafoDirigido<V,L>)g;
+		Arco<L> a = (Arco<L>)castedGraph.getEdges().get(id);
+		if (a == null) {
+			throw new NoSuchElementException();
+		}
+		return a;
+	}
 }
